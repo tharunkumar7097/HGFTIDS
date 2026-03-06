1b1535f5bf1d4d00a3a8f305d1b61bf4955ba89b
tharunkumar7097 <119399188+tharunkumar7097@users.noreply.github.com>
2026-03-06 23:34:47 +0530

    Add files via upload

---
+++ Hybrid_Graph_Fine_Tree_Intrusion_Detection_System/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Hybrid_Graph_Fine_Tree_Intrusion_Detection_System project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hybrid_Graph_Fine_Tree_Intrusion_Detection_System.settings')
+
+application = get_asgi_application()
